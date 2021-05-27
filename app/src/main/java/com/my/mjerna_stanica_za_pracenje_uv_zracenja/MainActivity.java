d6617b80db9c089760634775ceedd6f7f25e3ce0
Kolarka <tkolar@unipu.hr>
2021-05-27 17:22:56 +0200

    2

--- app/src/main/java/com/my/mjerna_stanica_za_pracenje_uv_zracenja/MainActivity.java
+++ app/src/main/java/com/my/mjerna_stanica_za_pracenje_uv_zracenja/MainActivity.java
@@ -1,14 +1,62 @@
 package com.my.mjerna_stanica_za_pracenje_uv_zracenja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends FragmentActivity {
+    private CircleImageView type1;
+    private CircleImageView type2;
+    private CircleImageView type3;
+    private CircleImageView type4;
+    private CircleImageView type5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        type1= (CircleImageView) findViewById(R.id.circle3);
+        type1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        type2= (CircleImageView) findViewById(R.id.circle1);
+        type2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        type3= (CircleImageView) findViewById(R.id.circle2);
+        type3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        type4= (CircleImageView) findViewById(R.id.circle4);
+        type4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        type5= (CircleImageView) findViewById(R.id.circle5);
+        type5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/my/mjerna_stanica_za_pracenje_uv_zracenja/UV.java
@@ -0,0 +1,14 @@
+package com.my.mjerna_stanica_za_pracenje_uv_zracenja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uv);
+    }
+}
